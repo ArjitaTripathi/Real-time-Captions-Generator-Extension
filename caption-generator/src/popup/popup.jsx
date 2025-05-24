e83162f630fa8d743c5fd67edad108cea76a957e
ArjitaTripathi <arjita461@gmail.com>
2025-05-24 23:48:51 +0530

    Initial commit

---
+++ caption-generator/src/popup/popup.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const popup = () => {
+
+  const startCaptioning = () => {
+
+  }
+  
+  return (
+    <div>
+        <h1>Start Captions</h1>
+        <button className={startCaptioning}>Start</button>
+    </div>
+  )
+}
+
+export default popup
